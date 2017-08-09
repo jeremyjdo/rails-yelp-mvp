2013f1b0b3295df5dfaf125c6481100aea4b8901
jeremy jodeau <jeremy.jodeau@gmail.com>
2017-08-09 11:41:59 +0200

    Add restaurant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phonenumber:  "0122686482",
+    category:     "french"
+  },
+  {
+    name:         "Mac Do",
+    address:      "la defense",
+    phonenumber:  "0288686482",
+    category:     "chinese"
+  },
+  {
+    name:         "Brigade",
+    address:      "56 rue de beaubourg 75012 Paris",
+    phonenumber:  "0122629482",
+    category:     "french"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
